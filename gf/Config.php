34c9578490296ef19fad80a576a2d92944e9dfb0
Nikolay <nikssa23@gmail.com>
2015-05-07 02:28:38 +0300

    change app.php config

--- gf/Config.php
+++ gf/Config.php
@@ -1,11 +1,35 @@
 <?php
-  namespace GF;
 
   /**
    * Description of Config
    * @author Nikolay Velchev <n.velchev95@gmail.com>
    */
+
+  namespace GF;
+
   class Config {
-      
+
+      private static $_instance = null;
+      private $_configFolder = null;
+      private $_configArray = array();
+
+      private function __construct() {
+	  
+      }
+
+      private function setConfigFolder($configFolder) {
+	  if (!$configFolder) {
+	      throw new \Exception('Empty config folder path:');
+	  }
+	  $_configFolder = realpath($configFolder);
+	  if ($_configFolder != FALSE && is_dir($_configFolder) && is_readable($_configFolder)) {
+	      //clear old config data
+	      $this->_configArray = array();
+	      $this->_configFolder = $_configFolder . DIRECTORY_SEPARATOR;
+	  } else {
+	      throw new \Exception('Culdnt directory read error:' . $_configFolder);
+	  }
+      }
+
   }
   
